6d6b27b39cb836e8f61b3474b801855f0b551fc0
Foxy <firebot1020@gmail.com>
2024-06-14 10:00:24 +0000

    Add Print

--- server-selector.py
+++ server-selector.py
@@ -187,6 +187,7 @@ async def main_loop(session, light_mode):
         except Exception as e:
             print(f"An error occurred: {e}")
             await asyncio.sleep(5*60)  # Wait before trying again
+            print("Restarting...")
             continue # Restart the loop
 
 async def main():
